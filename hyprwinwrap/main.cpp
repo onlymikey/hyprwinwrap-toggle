1ef6e98ad3d6e587b8d03aa756c2914f380be4df
Vaxry <vaxry@vaxry.net>
2026-02-23 16:57:22 +0000

    all: chase hyprland

--- hyprwinwrap/main.cpp
+++ hyprwinwrap/main.cpp
@@ -12,10 +12,11 @@
 #include <hyprland/src/desktop/view/Window.hpp>
 #include <hyprland/src/config/ConfigManager.hpp>
 #include <hyprland/src/render/Renderer.hpp>
-#include <hyprland/src/managers/LayoutManager.hpp>
 #include <hyprland/src/managers/input/InputManager.hpp>
 #include <hyprland/src/helpers/time/Time.hpp>
 #include <unordered_map>
+#include <hyprland/src/layout/LayoutManager.hpp>
+#include <hyprland/src/event/EventBus.hpp>
 #undef private
 
 #include "globals.hpp"
@@ -59,7 +60,7 @@ void                      onNewWindow(PHLWINDOW pWindow) {
         return;
 
     if (!pWindow->m_isFloating)
-        g_pLayoutManager->getCurrentLayout()->changeWindowFloatingMode(pWindow);
+        g_layoutManager->changeFloatingMode(pWindow->layoutTarget());
 
     float sx = 100.f, sy = 100.f, px = 0.f, py = 0.f;
 
@@ -244,12 +245,10 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
         throw std::runtime_error("[hww] Version mismatch");
     }
 
-    // clang-format off
-    static auto P  = HyprlandAPI::registerCallbackDynamic(PHANDLE, "openWindow", [&](void* self, SCallbackInfo& info, std::any data) { onNewWindow(std::any_cast<PHLWINDOW>(data)); });
-    static auto P2 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "closeWindow", [&](void* self, SCallbackInfo& info, std::any data) { onCloseWindow(std::any_cast<PHLWINDOW>(data)); });
-    static auto P3 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "render", [&](void* self, SCallbackInfo& info, std::any data) { onRenderStage(std::any_cast<eRenderStage>(data)); });
-    static auto P4 = HyprlandAPI::registerCallbackDynamic(PHANDLE, "configReloaded", [&](void* self, SCallbackInfo& info, std::any data) { onConfigReloaded(); });
-    // clang-format on
+    static auto P  = Event::bus()->m_events.window.open.listen([&](PHLWINDOW w) { onNewWindow(w); });
+    static auto P2 = Event::bus()->m_events.window.close.listen([&](PHLWINDOW w) { onCloseWindow(w); });
+    static auto P3 = Event::bus()->m_events.render.stage.listen([&](eRenderStage stage) { onRenderStage(stage); });
+    static auto P4 = Event::bus()->m_events.config.reloaded.listen([&] { onConfigReloaded(); });
 
     HyprlandAPI::addDispatcher(PHANDLE, "hyprwinwrap_toggle", dispatchToggleInteractivity);
 
